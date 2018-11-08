89be3707e038ad83c67dca75cb6c7bd6ec9a2618
camilla-m <camilla-m@outlook.com>
2018-11-08 21:07:42 -0200

    Trocando variável

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = http.listen(process.env.PORT || 3000);
 app.listen(port_number);
 
 app.get('/assets/css.css', function (req, res) {
